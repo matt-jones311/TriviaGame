dc6fa71cb4ec87792c654320c48b3f1d95fc731e
Matt Jones <mjon8807@yahoo.com>
2017-12-23 22:07:57 -0500

    2nd

---
+++ assets/javascript/app.js
@@ -0,0 +1,125 @@
+var startScreen;
+var gameHTML;
+var counter = 30;
+var questionArray = ["Whose alter ego is &quotBeercules&quot?", "If Ted and Barney opened a bar, what would they call it?", "Who was the subject of the hit song PS I Love you by Robin Sparkles?", "What does Ted steal for Robin on their first date?", "What does Barney do for a Living?", "Who helps Ted get his job as a college professor?", "Which actor narrates the show as present day Ted?", "Where does Ted meet the mother?"];
+var answerArray = [["Marshall", "Ted", "Lilly", "Barney"], ["Maclaren's","Puzzles","Barney's","Ted's"], ["Ted Mosby", "Simon Tremblay", "Paul Schaffer", "Alan Thicke"], ["Green Flute","Yellow Drum","Blue French Horn","Red Guitar"], ["International Business Man", "Stock Brocker","Lawyer", "P.L.E.A.S.E.",], ["Tony Grafanello","The Captain","Marshall","Hammond Druthers"], ["Bob Barker", "Bob Saget", "Bob Ross", "Bob Dylan"], ["Jail","A friend's apartment","A club on St. Patrick's Day","A Wedding"]];
+var imageArray = ["<img class='center-block img-right' src='assets/images/beercules.gif'>" , "<img class='center-block img-right' src='assets/images/puzzles.gif'>", "<img class='center-block img-right' src='assets/images/ps_iLoveYou.gif'>","<img class='center-block img-right' src='assets/images/bfh.gif'>", "<img class='center-block img-right' src='assets/images/please.gif'>"];
+var correctAnswers = ["A. Marshall", "B. Puzzles", "C. Paul Schaffer", "C. Blue French Horn", "D. P.L.E.A.S.E.", "A. Tony Grafanello", "B. Bob Saget", "D. A Wedding"];
+var questionCounter = 0;
+var selecterAnswer;
+var theClock;
+var correctTally = 0;
+var incorrectTally = 0;
+var unansweredTally = 0;
+
+
+
+$(document).ready(function() {
+
+
+function initialScreen() {
+	startScreen = "<p class='text-center main-button-container'><a class='btn btn-primary btn-lg btn-block start-button' href='#' role='button'>Start Quiz</a></p>";
+	$(".mainArea").html(startScreen);
+}
+
+initialScreen();
+
+//Create a function, generateHTML(), that is triggered by the start button, and generates the HTML seen on the project video...
+
+$("body").on("click", ".start-button", function(event){
+	event.preventDefault();
+	generateHTML();
+
+	timerWrapper();
+
+}); // Closes start-button click
+
+$("body").on("click", ".answer", function(event){
+	//answeredQuestion = true;
+	selectedAnswer = $(this).text();
+	if(selectedAnswer === correctAnswers[questionCounter]) {
+		//alert("correct");
+
+		clearInterval(theClock);
+		generateWin();
+	}
+	else {
+		//alert("wrong answer!");
+		clearInterval(theClock);
+		generateLoss();
+	}
+}); // Close .answer click
+
+$("body").on("click", ".reset-button", function(event){
+	clickSound.play();
+	resetGame();
+}); // Closes reset-button click
+
+});  //  Closes jQuery wrapper
+
+function generateLossDueToTimeOut() {
+	unansweredTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>You ran out of time!  The correct answer was: " + correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000);  //  change to 4000 
+}
+
+function generateWin() {
+	correctTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + correctAnswers[questionCounter] + "</p>" + imageArray[questionCounter];
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000);  //  change to 4000 
+}
+
+function generateLoss() {
+	incorrectTally++;
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: "+ correctAnswers[questionCounter] + "</p>" + "<img class='center-block img-wrong' src='img/x.png'>";
+	$(".mainArea").html(gameHTML);
+	setTimeout(wait, 4000); //  change to 4000 
+}
+
+function generateHTML() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. "+answerArray[questionCounter][1]+"</p><p class='answer'>C. "+answerArray[questionCounter][2]+"</p><p class='answer'>D. "+answerArray[questionCounter][3]+"</p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function wait() {
+	if (questionCounter < 7) {
+	questionCounter++;
+	generateHTML();
+	counter = 30;
+	timerWrapper();
+	}
+	else {
+		finalScreen();
+	}
+}
+
+function timerWrapper() {
+	theClock = setInterval(thirtySeconds, 1000);
+	function thirtySeconds() {
+		if (counter === 0) {
+			clearInterval(theClock);
+			generateLossDueToTimeOut();
+		}
+		if (counter > 0) {
+			counter--;
+		}
+		$(".timer").html(counter);
+	}
+}
+
+function finalScreen() {
+	gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+	$(".mainArea").html(gameHTML);
+}
+
+function resetGame() {
+	questionCounter = 0;
+	correctTally = 0;
+	incorrectTally = 0;
+	unansweredTally = 0;
+	counter = 30;
+	generateHTML();
+	timerWrapper();
+}
